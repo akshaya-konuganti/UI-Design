8a68ae897872026aece6fd94cd24b3546373f1f8
akshaya-konuganti <konugantiakshaya2005@gmail.com>
2025-03-13 13:44:14 +0530

    Add files via upload

--- LAB4/one.dart
+++ LAB4/one.dart
@@ -1,1 +1,12 @@
 
+import 'package:one/switchdemo.dart';
+import 'package:one/switchdemoo.dart';
+import 'dart:io';
+void main(List<String> arguments){
+	switchdemo sd1=switchdemo();
+	switchdemoo sd2=switchdemoo();
+	print("enter value:");
+	String x=stdin.readLineSync()!;
+	//sd1.fruits1(x);
+	print(sd2.fruits2(x));
+}
